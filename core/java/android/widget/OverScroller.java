b430a3464ea18092cb203d46cf3d0539f46f0827
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-03-30 19:18:53 +0800

    [bugfix] fixing ax overscroller issue
    issue https://github.com/AxionAOSP/issue_tracker/issues/83
    
    Change-Id: If16355b1f12dec93c8266ec3d9d63b1999471fd4

--- core/java/android/widget/OverScroller.java
+++ core/java/android/widget/OverScroller.java
@@ -706,6 +706,9 @@ public class OverScroller {
         void setFinalPosition(int position) {
             mFinal = position;
             mSplineDistance = mFinal - mStart;
+            if (mUseSpringPhysics) {
+                mSpringDistanceOld = mSplineDistance;
+            }
             mFinished = false;
         }
 
