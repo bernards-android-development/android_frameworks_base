645cf48a319d7ed44d8f356761504b4bdd7055e3
hzj5158888 <www1693303798@163.com>
2026-04-13 00:26:12 +0800

    core: process fixup
    Change-Id: Ic534a5dfb483836ab6f56fdc045fe7ffd9994ad9

--- core/jni/android_util_Process.cpp
+++ core/jni/android_util_Process.cpp
@@ -420,6 +420,11 @@ static void get_cpuset_cores_for_policy(SchedPolicy policy, cpu_set_t *cpu_set)
                 return;
             }
             break;
+        case SP_SVP:
+            if (!CgroupGetAttributePath("SvpCPUs", &filename)) {
+                return;
+            }
+            break;
         case SP_FOREGROUND_WINDOW:
             if (!CgroupGetAttributePath("HighCapacityWICPUs", &filename)) {
                 return;
