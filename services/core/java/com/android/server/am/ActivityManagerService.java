bbb277d66f19c80cd8b19846c07d3a899ee5d4dc
hzj5158888 <www1693303798@163.com>
2026-05-12 16:56:49 +0800

    Revert "fix getProcessPss"
    This reverts commit 9eb7784b31780c82a0844c5306881f8c949bda39.
    
    Change-Id: Ib653cddb3b8026e8c25564aa8e5e18f6011a849f

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -4143,6 +4143,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         enforceNotIsolatedCaller("getProcessPss");
 
         final long[] pss = new long[pids.length];
+
         if (!Build.IS_ENG) {
             return pss;
         }
@@ -4156,6 +4157,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         final boolean allUids = mAtmInternal.isGetTasksAllowed(
                 "getProcessPss", callingPid, callingUid);
 
+        final long[] pss = new long[pids.length];
         for (int i=pids.length-1; i>=0; i--) {
             ProcessRecord proc;
             int oomAdj;
