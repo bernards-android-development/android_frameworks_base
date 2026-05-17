9a598b66f7ecb3fd3a53bc8bc126ab0286f61b80
hzj5158888 <www1693303798@163.com>
2026-05-17 14:07:54 +0800

    Revert "[ax_freezer] adding process freezer manager"
    This reverts commit 2b40e3ceeb75359fa41e9f9582aa83969cac6cb8.
    
    Change-Id: I21c606fe064eefa4b5b3d8ee4327b2c6c4e6b945

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -988,10 +988,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         final int pid = app.getPid();
         synchronized (mPidsSelfLocked) {
             mPidsSelfLocked.doAddInternal(pid, app);
-            ProcessFreezerManager freezer = ProcessFreezerManager.getInstance();
-            if (freezer != null && freezer.useFreezerManager()) {
-                freezer.addPidLocked(app);
-            }
         }
         synchronized (sActiveProcessInfoSelfLocked) {
             if (app.processInfo != null) {
@@ -1013,11 +1009,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         final boolean removed;
         synchronized (mPidsSelfLocked) {
             removed = mPidsSelfLocked.doRemoveInternal(pid, app);
-            ProcessFreezerManager freezer = ProcessFreezerManager.getInstance();
-            if (freezer != null && freezer.useFreezerManager()) {
-                freezer.removePidLocked(pid, app);
-                freezer.startUnfreeze(app.processName, ProcessFreezerManager.REMOVE_PROCESS_UNFREEZE);
-            }
         }
         if (removed) {
             synchronized (sActiveProcessInfoSelfLocked) {
@@ -5411,7 +5402,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         showMteOverrideNotificationIfActive();
 
         t.traceEnd();
-        ProcessFreezerManager.getInstance().init(mFreezer);
     }
 
     private void showConsoleNotificationIfActive() {
